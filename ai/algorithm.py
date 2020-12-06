0807cf9513f738dd71916a61d81c6cd38af5b2b9
Peltea Bogdan <mhibogdan@gmail.com>
2020-12-06 20:02:13 +0200

    Ai implementation : Medium , Hard

---
+++ ai/algorithm.py
@@ -0,0 +1,143 @@
+import pygame
+from copy import deepcopy
+from entities.constants import BROWN, GREY, ROWS, COLS
+
+def heuristicFunction(copyTable):
+    curr_x = copyTable.mouseX 
+    curr_y = copyTable.mouseY
+    min_list = []
+    count_up, count_down, count_left, count_right, count_up_left, count_up_right, count_down_left, count_down_right = 0,0,0,0,0,0,0,0
+    
+    for up in range(curr_x):
+        if copyTable.board[up][curr_y] == 0:
+                count_up += 1
+        else:
+            # count_up = 0
+            count_up = float('inf')
+            break
+    min_list.append(count_up)
+    
+    for down in range(curr_x + 1, ROWS):
+        if copyTable.board[down][curr_y] == 0:
+                count_down += 1
+        else:
+            # count_down = 0 
+            count_down = float('inf')
+            break
+    min_list.append(count_down)
+
+    for left in range(curr_y):
+        if copyTable.board[curr_y][left] == 0:
+                count_left += 1
+        else:
+            # count_left = 0
+            count_left = float('inf')
+            break
+    min_list.append(count_left)
+
+    for right in range(curr_y + 1, COLS):
+        if copyTable.board[curr_x][right] == 0:
+                count_right += 1
+        else:
+            # count_right = 0 
+            count_right = float('inf')
+            break
+    min_list.append(count_right)
+    
+    for up_left in range(1, ROWS):
+        if copyTable.board[curr_x - up_left][curr_y - up_left] == 0:
+                count_up_left += 1
+        else:
+            # count_up_left = 0 
+            count_up_left = float('inf')
+            break
+        if (curr_x - up_left) == 0 or (curr_y - up_left) == 0:
+            break
+    min_list.append(count_up_left)
+
+    for up_right in range(1, ROWS):
+        if (curr_x - up_right ) == 0 or (curr_y + up_right) == COLS:
+            break
+
+        if copyTable.board[curr_x - up_right][curr_y + up_right] == 0:
+                count_up_right += 1
+        else:
+            # count_up_right = 0
+            count_up_right = float('inf')
+            break
+    min_list.append(count_up_right)
+
+    for down_left in range(1, ROWS):
+        if (down_left + curr_x) == ROWS or (curr_y - down_left) == 0:
+            break
+
+        if copyTable.board[curr_x + down_left][curr_y - down_left] == 0:
+                count_down_left += 1
+        else:
+            # count_down_left = 0
+            count_down_left = float('inf')
+            break
+    min_list.append(count_down_left)
+
+    for down_right in range(1, ROWS):
+        if (down_right + curr_x) == ROWS or (down_right + curr_y) == COLS:
+            break
+        
+        if copyTable.board[curr_x + down_right][curr_y + down_right] == 0:
+                count_down_right += 1
+        else:
+            # count_down_right = 0
+            count_down_right = float('inf')
+            break
+    min_list.append(count_down_right)
+    # sum_available = count_up + count_down + count_left + count_right + count_up_left + count_up_right + count_down_left + count_down_right
+    return min(min_list)
+    
+
+def ai_move(tableTest, depth, max_turn):
+    if depth == 0 or tableTest.winner() != "False":
+        return heuristicFunction(tableTest),tableTest
+    
+    if max_turn:
+        maxEval = float('inf')
+        best_move = None
+        
+        for move in get_available_moves(tableTest, GREY): 
+            evaluation = ai_move(move, depth-1, True)[0]
+            maxEval = min(maxEval, evaluation)
+            if maxEval == evaluation:
+                best_move = move 
+        return (maxEval, best_move)
+    
+    # else:
+    #     minEval = float('inf')
+    #     best_move = None
+        
+    #     for move in get_available_moves(tableTest, BROWN): 
+    #         evaluation = minimax(move, depth-1, True)[0]
+    #         minEval = min(minEval, evaluation)
+    #         if minEval == evaluation:
+    #             best_move = move
+    #     return (minEval, best_move)
+
+
+def get_available_moves(Board, color):
+    moves = []
+    if color == GREY:
+        for x in [-1,0,1]:
+                for y in [-1,0,1]:
+                    if(Board.board[Board.mouseX + x][Board.mouseY + y]) == 0:
+                        aux_board = deepcopy(Board)
+                        aux_board.move((Board.mouseX + x) , (Board.mouseY + y))
+                        moves.append(aux_board)
+    else:
+        for row in range(ROWS):
+            for col in range(COLS):
+                if Board.board[row][col] == 0:
+                    aux_board = deepcopy(Board)
+                    aux_board.add(row,col)
+                    moves.append(aux_board)
+    return moves
+    
+                        
+    
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pygame
 from entities.constants import WIDTH, HEIGHT, SQUARE_SIZE, DARK_GREEN, GREY, BROWN
 from entities.game import Game
 from ai.minmax import minimax
-
+from ai.minmax_alpha_beta import minimaxAlpha_Beta
+from ai.algorithm import ai_move
 
 FPS = 60
 
@@ -20,15 +21,26 @@ def main():
     run = True
     clock = pygame.time.Clock()
     game = Game(WIN)
+    # 0 - without ai
+    # 1 - easy
+    # 2 - medium
+    # 3 - hard
+    inputLevel = int(input("Select the difficulty:"))
+
     while run:
         clock.tick(FPS)
         
+        if inputLevel in range(1,4):
+            if game.turn == GREY:
+                if inputLevel == 1:
+                    value, new_board = minimax(game.get_board(), 2, True)
+                if inputLevel == 2:
+                    value, new_board = minimaxAlpha_Beta(game.get_board(), 2, float('-inf'),float('inf'), True)
+                if inputLevel == 3:
+                    value, new_board = ai_move(game.get_board(), 3, True)
+                game.ai_move(new_board)
+            
 
-        if game.turn == GREY:
-            value, new_board = minimax(game.get_board(), 2, True)
-            game.ai_move(new_board)
-             
-             
         if game.winner() != "False":
             print(game.winner())
             run = False
